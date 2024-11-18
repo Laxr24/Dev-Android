3961993f8e4b1d0ccea1ecb2c7c62b84afaf8988
Laxr24 <shahanuzzamans@gmail.com>
2024-11-18 13:03:41 +0600

    Update

--- Learning-Android/app/src/main/java/com/example/learningandroid/MainActivity.java
+++ Learning-Android/app/src/main/java/com/example/learningandroid/MainActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -27,7 +30,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
-
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                        requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, 111);
+                    } else {
+                        //Open call function
+                        String phone = "7769942159";
+                        Intent intent = new Intent(Intent.ACTION_CALL);
+                        intent.setData(Uri.parse("tel:+91" + phone));
+                        startActivity(intent);
+                    }
+                }else{
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CALL_PHONE}, 111);
                 }
             }
         });
